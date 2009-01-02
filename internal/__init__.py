e9d9d4d94d2af399387dfad9edff8aace9a676d1
R. Tyler Ballance <tyler@slide.com>
2009-01-01 16:00:05 -0800

    Pulling committer details out, working on committers graph first

---
+++ internal/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+def execute(cmd, *args, **kwargs):
+	kwargs = kwargs or {}
+	args = args or []
+	kwargs = ['--%s=%s' % (k,v) for k,v in kwargs.iteritems()]
+	rc = os.popen('git %s %s %s' % (cmd, ' '.join(args), ' '.join(kwargs)))
+	rc = rc.read().rstrip()
+	return rc.split('\n')
+	
